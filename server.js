65a20998c5f41723731c59f2a4edf333e677186b
nonamename66 <86437348+nonamename66@users.noreply.github.com>
2021-06-24 20:47:00 +0000

    Update server.js

--- server.js
+++ server.js
@@ -30,18 +30,7 @@ cors_proxy.createServer({
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
+    'aaaaa',
   ],
   redirectSameOrigin: true,
   httpProxyOptions: {
