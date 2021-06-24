0e0bb4e53d42f5aead5f34747bb56ddc116158da
nonamename66 <86437348+nonamename66@users.noreply.github.com>
2021-06-24 21:57:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlockList: originBlockList,
-  originAllowList: originAllowList,
+  originAllowList: ['xcoins.com', 'localbitcoins.com'],
   checkRateLimit: checkRateLimit,
   removeHeaders: ['cookie', 'cookie2'],
   redirectSameOrigin: true,
